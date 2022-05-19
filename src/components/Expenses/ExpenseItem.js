9a919ad0cc3c2d385c3d97bfee0f168d0bdbdb76
Tommaso Betti <tommaso.betti@nttdata.com>
2022-05-19 12:51:37 +0200

    wip on lists display

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,7 +13,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
+      <button onClick={props.clickHandler}>Change title</button>
     </Card>
   );
 };

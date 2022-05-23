b79ea58b070497b3d700b6e401e3d9474c7df2eb
Tommaso Betti <tommaso.betti@nttdata.com>
2022-05-23 17:57:50 +0200

    initial commit core practice sub-project

--- src/App.js
+++ src/App.js
@@ -1,65 +1,12 @@
-import React, { useState } from "react";
-import "./App.css";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React from 'react';
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
 
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-  // add incoming expense as first element and mantain old array of expenses
-  // wrong way -> you should use special function form
-  // const addExpenseHandler = (expense) => {
-  //   setExpenses([expense, ...expenses]);
-  // };
-
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  };
-
-  // what is happening under the hood with below syntax
-  // older versions of react needed React import where jsx was used
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement(),
-  //   React.createElement()
-  // )
-
+function App() {
   return (
-    <div className="App">
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+    <div>
+
     </div>
   );
-};
+}
 
 export default App;

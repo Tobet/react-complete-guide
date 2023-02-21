86b85cb307cbc571e7ce2b286830f1e1a2590ce0
Tommaso Betti <tommaso.betti@nttdata.com>
2023-02-21 12:02:11 +0100

    fragments to solve div soup

--- src/App.js
+++ src/App.js
@@ -1,65 +1,26 @@
 import React, { useState } from "react";
-import "./App.css";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+function App() {
+  const [usersList, setUsersList] = useState([]);
 
-  // add incoming expense as first element and mantain old array of expenses
-  // wrong way -> you should use special function form
-  // const addExpenseHandler = (expense) => {
-  //   setExpenses([expense, ...expenses]);
-  // };
-
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
 
-  // what is happening under the hood with below syntax
-  // older versions of react needed React import where jsx was used
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement(),
-  //   React.createElement()
-  // )
-
   return (
-    <div className="App">
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
-    </div>
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
   );
-};
+}
 
 export default App;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,9 @@
-import "./Card.css";
+import React from 'react';
+
+import classes from './Card.module.css';
 
 const Card = (props) => {
-  const classes = "card " + props.className; // attach classes used on component tag
-
-  return <div className={classes}>{props.children}</div>;
+  return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
 };
 
 export default Card;
